a4102806d8364b21a54785aeaae64601b72a5eb9
Daniel Rubio <daniel@mingadigital.com>
2021-03-03 21:33:51 -0300

    modify serve, promise and await

--- server.js
+++ server.js
@@ -1,164 +1,174 @@
 const express = require('express');
 const app = express();
 const https = require('https');
+const querystring = require('querystring'); 
 var cors = require('cors');
-
+ 
 app.listen(8000, () => {
     console.log('Server started!')
-}) 
+});
+
 app.use(cors({origin: 'http://localhost:4200'}));
 
 
-app.get("/api/items", (req, res, next) => { 
-    
-    let params = req.query;
-    let query_srt = "";
-      
-    Object.keys(params).forEach(function eachKey(key) {
-        query_srt += '?' + key + '=' + params[key] +'&limit=4';   
-    });  
  
-    https.get('https://api.mercadolibre.com/sites/MLA/search' + query_srt, function(response) {
-        var body = ''; 
-        response.on('data', function(chunk) {
-          body += chunk;
-        });
+function httpRequest(options){
+    return new Promise ((resolve, reject) => {  
+        let req = https.request(options, (res) => {  
+            let body = [];
 
-        response.on('end', function() {  
-            let most_frecuency =  '';           
-            let item_array = [];
-            let categories_array = []; 
-            body = JSON.parse(body); 
-
-            body.results.forEach(element => { 
-                let item = {
-                    "id":  element.id,
-                    "title": element.title,
-                    "price": {
-                        "currency": element.currency_id,
-                        "amount": element.price,
-                        "decimals": 0
-                    },
-                    "picture": element.thumbnail,
-                    "condition": element.condition,
-                    "free_shipping": element.shipping.free_shipping 
+            res.on('data', (d) => {
+                body.push(d);
+            }).on('end', () => {
+                try {
+                    body = Buffer.concat(body).toString();
+                } catch(e) {
+                    reject(e);
                 }
-                item_array.push(item);  
-                categories_array.push(element.category_id); 
-            });
-            
-            most_frecuency = findCommon(categories_array); 
-            
-            categories_array = []; +
-            https.get('https://api.mercadolibre.com/categories/'+most_frecuency, (respo) => { 
-                respo.on('data', (d) => { 
-                    body = JSON.parse(d);  
-                    body.path_from_root.forEach(category => { 
-                        categories_array.push(category.name); 
-                    });   
-                 
-                    let return_response = {
-                        "author": { "name": "Daniel", "lastname": "Rubio"},
-                        "items" : item_array,
-                        "categories" : categories_array
-                    } 
-        
-                    res.json(return_response); 
 
-                }); 
-            
-            }).on('error', (e) => {
-                console.error(e);
-            }); 
-             
+                resolve(JSON.parse(body));
+            });
         });
         
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
-    }); 
+        req.on('error', (e) => { 
+            reject(e);
+        }); 
 
-    function findCommon(arr) {
-        var max = 1,
-            m = [],
-            val = arr[0],
-            i, x;
-    
-        for(i = 0; i < arr.length; i ++) {
-            x = arr[i]
-            if (m[x]) {
-                ++m[x] > max && (max = m[i], val = x);
-            } else {
-                m[x] = 1;
-            }
-        } return val;    
+        req.end();  
+    }) 
+} 
+
+app.get('/api/items', async (req, res, next)  => { 
+    let params = req.query;
+    let return_response = {
+        "author": { "name": "Daniel", "lastname": "Rubio"},
+        "items" : [],
+        "categories" : []
+    };
+    let options = { 
+        hostname: 'api.mercadolibre.com',
+        port: 443,
+        path: '/sites/MLA/search?',
+        method: 'GET', 
+    };
+
+    if(Object.keys(params).length > 0){
+        options.path = '/sites/MLA/search?' + querystring.stringify(params) + '&limit=4';
     }
       
-   
-});
 
-
-app.get("/api/items/:id", (req, res, next) => {  
-    let body = '';
-    https.get('https://api.mercadolibre.com/items/' + req.params.id, function(response) {
-        let data = [];
-
-        response.on('data', function(chunk) {
-            data.push(chunk);
-        }).on('end', function() { 
-            let buffer = Buffer.concat(data);
-            data = JSON.parse(buffer);   
-
-            https.get('https://api.mercadolibre.com/items/' + req.params.id+'/description', function(resp) {
-                let data_description = []; 
-
-                resp.on('data', function(chunk) {
-                    data_description.push(chunk);
-                }).on('end', function() {
-                    let buffer = Buffer.concat(data_description);
-                    data_description = JSON.parse(buffer);   
-
-                    https.get('https://api.mercadolibre.com/categories/'+data.category_id, (respo) => { 
-                        let data_category = []; 
-                        let categories_array = [];
-
-                        respo.on('data', function(chunk) {
-                            data_category.push(chunk);
-                        }).on('end', function() {
-                            body = JSON.parse(data_category);  
-                            body.path_from_root.forEach(category => { 
-                                categories_array.push(category.name); 
-                            });   
-                            console.log(body);
-
-                            let response_data = {
-                                "author": { "name": "Daniel", "lastname": "Rubio"},
-                                "item" : {
-                                    "id":  data.id,
-                                    "title": data.title,
-                                    "price": {
-                                        "currency": data.currency_id,
-                                        "amount": data.price,
-                                        "decimals": 0
-                                    },
-                                    "picture": data.pictures[0].url,
-                                    "condition": data.condition,
-                                    "free_shipping": data.shipping.free_shipping,
-                                    "sold_quantity": data.sold_quantity,
-                                    "description": data_description.plain_text 
-                                },
-                                "categories" : categories_array
-                            }  
-                    
-                            res.json(response_data);
-
-                        }) 
-                    }); 
-                    
-                }) 
- 
-            });
-        });
+    httpRequest(options).then(response => {
+        let items = []; 
+        let categories = [];
+        let category_most_frecuently = '';
         
+        if('results' in response){ 
+
+            response.results.map((item) => { 
+
+                let new_item = {
+                    "id":  item.id,
+                    "title": item.title,
+                    "price": {
+                        "currency": item.currency_id,
+                        "amount": item.price,
+                        "decimals": 0
+                    },
+                    "picture": item.thumbnail,
+                    "condition": item.condition,
+                    "free_shipping": item.shipping.free_shipping 
+                }  
+
+                items.push(new_item); 
+
+                if(item.category_id in categories){
+                    categories[item.category_id] ++
+                } else {
+                    categories[item.category_id] = 0
+                }  
+
+            });
+            
+            return_response.items = items;
+            categories.sort();
+            category_most_frecuently = Object.keys(categories)[0];
+            options.path = '/categories/'+category_most_frecuently 
+
+            httpRequest(options).then(response => {
+                categories = [];
+                response.path_from_root.map((category) => { 
+                    categories.push(category.name);
+                }) 
+                return_response.categories = categories;
+
+                res.json(return_response);
+
+            }).catch(error => console.error(error));  
+        }
+        
+
+    }).catch(error => console.error(error));
+ 
+}); 
+ 
+
+app.get("/api/items/:id", async (req, res, next) => {
+    let params = req.params; 
+    let return_response = {
+        "author": { "name": "Daniel", "lastname": "Rubio"},
+        "item" : {},
+        "categories" : []
+    }  
+    let options = { 
+        hostname: 'api.mercadolibre.com',
+        port: 443,
+        path: '/items/',
+        method: 'GET', 
+    };
+    let options2 = { 
+        hostname: 'api.mercadolibre.com',
+        port: 443,
+        path: '/items/',
+        method: 'GET', 
+    };
+    let options3 = { 
+        hostname: 'api.mercadolibre.com',
+        port: 443,
+        path: '/items/',
+        method: 'GET', 
+    };
+    
+    if('id' in params) {
+        options.path = '/items/'+params.id; 
+        options2.path = '/items/' + params.id + '/description'; 
+    }
+ 
+    await httpRequest(options).then( response => { 
+        options3.path = '/categories/'+response.category_id;
+        return_response.item = {
+            "id":  response.id,
+            "title": response.title,
+            "price": {
+                "currency": response.currency_id,
+                "amount": response.price,
+                "decimals": 0
+            },
+            "picture": response.pictures[0].url,
+            "condition": response.condition,
+            "free_shipping": response.shipping.free_shipping,
+            "sold_quantity": response.sold_quantity
+        }  
+    })
+     
+    await httpRequest(options2).then( response => {
+        return_response.item.description = response.plain_text; 
     })
 
-})
+    await httpRequest(options3).then( response => { 
+        return_response.categories = response.path_from_root;  
+    })
+
+    res.json(return_response); 
+}) 
+
+ 
